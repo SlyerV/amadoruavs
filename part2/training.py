0706d70d275fed918fad14e18b593d51d9d8b47b
SlyerV <80004341+SlyerV@users.noreply.github.com>
2025-09-17 03:51:49 -0700

    Update training.py

--- part2/training.py
+++ part2/training.py
@@ -5,5 +5,5 @@ from ultralytics import YOLO
 baseDir = os.path.abspath(os.path.dirname(__file__))
 dataPath = os.path.join(baseDir, "data.yaml")
 
-model = YOLO('yolov8n.pt') # Loads pretrained YOLOv8n model
+model = YOLO('yolov8n.pt')
 model.train(data=dataPath, epochs=50, imgsz=320, batch=4, exist_ok=True) # Used low imgsz and batch for faster training on codespace - feel free to change parameters
